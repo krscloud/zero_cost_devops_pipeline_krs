103fc6c5a0eb63096bbadfa50a0781179ce279d5
Oraclekrs <oraclekrscloud@gmail.com>
2026-05-22 05:49:49 +0530

    just made some changes to the application

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>KRS DevOps Pipeline: Operational</h1><p>Triggered via GitHub Actions & Ansible.</p>"
+    return "<h1> Just making changes to see if the app is working as expected.
+
+KRS First Ever DevOps Pipeline: Operational</h1><p>Triggered via GitHub Actions & Ansible.</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
