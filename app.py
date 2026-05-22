b809f70724e3a7a30b795f73e948e44b49b8c4a6
Oraclekrs <oraclekrscloud@gmail.com>
2026-05-23 02:03:50 +0530

    new changes to app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return """<h1> Testing the 2nd time
+    return """<h1> Testing the 3rd time, it better work
 KRS First Ever DevOps Pipeline: Operational</h1><p>Triggered via GitHub Actions & Ansible.</p>"""
 
 if __name__ == '__main__':
